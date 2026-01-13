153e16f657a72f80c360cf3344ec7a1c748ea081
SmithFerreira-07 <GitFerreira@proton.me>
2026-01-13 13:04:58 -0300

    first solution

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,16 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Create a new array of doubles with the size of length
+        double[] multiplesof = new double[length];
+        //Use a for loop to iterate through the array
+        for (int i = 0; i < length; i++)
+        {
+            //Set each element in the array to be number multiplied by (i + 1)
+            multiplesof[i] = number * (i + 1);
+        }
+
+        return multiplesof; // replace this return statement with your own
     }
 
     /// <summary>
